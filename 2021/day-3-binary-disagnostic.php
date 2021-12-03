a696c0c6539ff5640e58bd3f4cf87987d1f63f94
Jon Campbell <jcampbell@predictiveindex.com>
2021-12-03 13:23:10 -0500

    Day 3, Part 2!

--- 2021/day-3-binary-disagnostic.php
+++ 2021/day-3-binary-disagnostic.php
@@ -6,29 +6,48 @@ $diagnosticReport = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "day-3-inp
 
 $diagnosticReport = trim($diagnosticReport);
 
-$reportLines = explode(PHP_EOL, $diagnosticReport);
-$reportLines = array_map('trim', $reportLines);
+function getRating(string $diagnosticReport, bool $seekMostCommonBit = true, int $tieBreakerWinValue = 1)
+{
+    $tieBreakerLoserValue = ($tieBreakerWinValue == 1 ? 0 : 1);
 
-$reportCharacters = array_map('str_split', $reportLines);
+    $reportLines = explode(PHP_EOL, $diagnosticReport);
+    $reportLines = array_map('trim', $reportLines);
 
-$gammaRateBinary = "";
-$epsilonRateBinary = "";
+    $mask = "";
+    $rating = "";
 
+    for ($i = 0; $i < strlen($reportLines[0]); $i++) {
 
-for ($i = 0; $i < strlen($reportLines[0]); $i++) {
+        $reportCharacters = array_map('str_split', $reportLines);
+    
+        $positionValues = array_column($reportCharacters, $i);
+        $countValues = array_count_values($positionValues);
 
-    $positionValues = array_column($reportCharacters, $i);
-
-    $countValues = array_count_values($positionValues);
-
-    if ($countValues[1] > $countValues[0]) {
-        $gammaRateBinary .= 1;
-        $epsilonRateBinary .= 0;
-    } else {
-        $gammaRateBinary .= 0;
-        $epsilonRateBinary .= 1;
+        if ($countValues[$tieBreakerWinValue] == $countValues[$tieBreakerLoserValue]) {
+            $mask .= $tieBreakerWinValue;
+        } else if ($seekMostCommonBit) {
+            $mask .= array_search(max($countValues), $countValues, true);
+        } else {
+            $mask .= array_search(min($countValues), $countValues, true);
+        }
+    
+        $matches = [];
+        preg_match_all("/\b{$mask}.*\b/i", $diagnosticReport, $matches);
+    
+        $reportLines = $matches[0];
+    
+        if (sizeof($reportLines) == 1) {
+            $rating = $reportLines[0];
+            break;
+        }    
     }
 
+    return $rating;
 }
 
-print "Gamma: " . bindec($gammaRateBinary) . " Epsilon: " . bindec($epsilonRateBinary) . " (" . bindec($gammaRateBinary) * bindec($epsilonRateBinary) . ")";
+$oxygenRating = getRating($diagnosticReport, true, 1);
+$co2rating = getRating($diagnosticReport, false, 0);
+
+
+print "Oxygen: " . $oxygenRating . " CO2: " . $co2rating . PHP_EOL;
+print "Oxygen: " . bindec($oxygenRating) . " CO2: " . bindec($co2rating) . " (" . bindec($oxygenRating) * bindec($co2rating) . ")";
