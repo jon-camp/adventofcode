06c3d0a0ad4ddb583813abb439165fc9d51e24e5
Jon Campbell <jcampbell@predictiveindex.com>
2021-12-10 16:38:38 -0500

    Day 7, Part 1!

---
+++ 2021/day-7-treachery-of-whales.php
@@ -0,0 +1,35 @@
+<?php
+
+$input = "16,1,2,0,4,2,7,1,2,14";
+$input = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "day-7-input.txt");
+
+
+$crabPositions = explode(",", $input);
+$crabPositions = array_map('trim', $crabPositions);
+
+$minPosition = min($crabPositions);
+$maxPosition = max($crabPositions);
+
+$crabsByPosition = array_fill($minPosition, $maxPosition + 1, 0);
+$fuelByPositon = array_fill($minPosition, $maxPosition + 1, 0);
+
+foreach ($crabPositions as $position) {
+    $crabsByPosition[$position]++;
+}
+
+foreach ($fuelByPositon as $targetPositon => $fuel) {
+
+    $fuel = 0;
+
+    foreach ($crabsByPosition as $fromPosition => $crabs) {
+
+        $fuel += abs($targetPositon - $fromPosition) * $crabs;
+    }
+
+    $fuelByPositon[$targetPositon] = $fuel;
+}
+
+print_r($fuelByPositon);
+$minFuel = min($fuelByPositon);
+
+print "Min: {$minFuel} at Position: " . array_search($minFuel, $fuelByPositon) . PHP_EOL;
