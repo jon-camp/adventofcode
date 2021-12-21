b4ada2e9c24fef484922671f42f918a6b120cd65
Jon Campbell <jcampbell@predictiveindex.com>
2021-12-21 17:21:08 -0500

    Day 21, Part 1 w/ Sequences

--- 2021/day-21-dirac-dice.php
+++ 2021/day-21-dirac-dice.php
@@ -2,7 +2,7 @@
 
 $input="Player 1 starting position: 4
 Player 2 starting position: 8";
-$input = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "day-21-input.txt");
+//$input = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "day-21-input.txt");
 
 $lines = explode(PHP_EOL, $input);
 
@@ -14,30 +14,84 @@ $player2Score = 0;
 
 $targetScore = 1000;
 
-$nextRoll = 1;
-$totalRolls = 0;
+$rollSequences = [[]];
+
+$player1Wins = 0;
+$player2Wins = 0;
 
 
-while ($player1Score < $targetScore && $player2Score < $targetScore) {
+do {
 
-    
-    $player1Roll = getPlayerRole();
-    $player1Position = getNewPosition($player1Position, $player1Roll);
-    $player1Score += $player1Position;
+    $rollSequences = addThreeRolls($rollSequences);
 
-    if ($player1Score >= $targetScore) {
-        break;
+    foreach ($rollSequences as $r => $rollSequence) {
+        list($player1Score, $player2Score) = getPlayerScoresFromSequence($rollSequence);
+
+        //print "Player 1: {$player1Score} Player 2: {$player2Score}" . PHP_EOL;
+
+
+        if ($player1Score >= $targetScore) {
+            $player1Wins++;
+
+            unset($rollSequences[$r]);
+        }
     }
 
-    $player2Roll = getPlayerRole();
-    $player2Position = getNewPosition($player2Position, $player2Roll);
-    $player2Score += $player2Position;
+    $rollSequences = addThreeRolls($rollSequences);
+
+    foreach ($rollSequences as $r => $rollSequence) {
+        list($player1Score, $player2Score) = getPlayerScoresFromSequence($rollSequence);
+
+        if ($player2Score >= $targetScore) {
+            $player2Wins++;
+            unset($rollSequences[$r]);
+        }
+    }
+
+} while (sizeof($rollSequences));
+
+print "Player 1: {$player1Wins}" . PHP_EOL;
+print "Player 2: {$player2Wins}" . PHP_EOL;
+
+
+function getPlayerScoresFromSequence(array $rollSequence) {
+    
+    global $player1Position, $player2Position;
+    global $targetScore;
+
+    // Don't change the global values.
+    $player1 = $player1Position;
+    $player2 = $player2Position;
+
+    $player1Score = 0;
+    $player2Score = 0;
+
+    while (true) {
+
+        $nextRoll = array_shift($rollSequence);
+        $nextRoll += array_shift($rollSequence);
+        $nextRoll += array_shift($rollSequence);
+
+        $player1 = getNewPosition($player1, $nextRoll);
+        $player1Score += $player1;
+
+
+        if (empty($rollSequence) || $player1Score >= $targetScore) {
+            return [$player1Score, $player2Score];
+        }
+
+        $nextRoll = array_shift($rollSequence);
+        $nextRoll += array_shift($rollSequence);
+        $nextRoll += array_shift($rollSequence);    
+
+        $player2 = getNewPosition($player2, $nextRoll);
+        $player2Score += $player2;
+
+        if (empty($rollSequence) || $player2Score >= $targetScore) {
+            return [$player1Score, $player2Score];
+        }    
+    }
 }
-
-$losingScore = min([$player1Score, $player2Score]);
-
-print "Losing Score {$losingScore} * Total Rolls {$totalRolls} = " . ($losingScore * $totalRolls) . PHP_EOL;
-
 
 function getNewPosition($current, $roll) 
 {
@@ -51,25 +105,29 @@ function getNewPosition($current, $roll)
 
 
 
-function getPlayerRole()
+function addThreeRolls(array $rollSequences)
 {
-    global $nextRoll;
-    global $totalRolls;
+    foreach ($rollSequences as $r => $rollSequence) {
 
-    $total = 0;
-
-    for ($i = 0; $i < 3; $i++) {
-        $roll = $nextRoll;
-        $totalRolls++;
-
-        $total += $roll;
-
-        if ($roll == 100) {
-            $nextRoll = 1;
+        if (empty($rollSequence)) {
+            $lastRoll = 0;
         } else {
-            $nextRoll++;
+            $lastRoll = array_pop($rollSequence);
+            array_push($rollSequence, $lastRoll);
         }
+    
+        for ($i = 1; $i <= 3; $i++) {
+            $roll = $lastRoll + $i;
+    
+            if ($roll > 100) {
+                $roll -= 100;
+            }
+    
+            array_push($rollSequence, $roll);
+        }
+    
+        $rollSequences[$r] = $rollSequence;
     }
 
-    return $total;
+    return $rollSequences;
 }
