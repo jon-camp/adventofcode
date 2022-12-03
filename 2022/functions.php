0d3dc87dd6bb3f87c9a69267791b229b07474463
Jonathan Campbell <jon@healsdata.com>
2022-12-02 23:45:02 -0500

    2022 Day 2

---
+++ 2022/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function fgetsGenerator($file)
+{
+    $handle = fopen($file, "r");
+    if (!$handle) {
+        throw new \InvalidArgumentException($file);
+    }
+
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        yield trim($buffer);
+    }
+
+    fclose($handle);
+}
+
+
+function inputGenerator($lines)
+{
+    $lines = explode(PHP_EOL, $lines);
+
+    foreach ($lines as $line) {
+        yield trim($line);
+    }
+
+}
